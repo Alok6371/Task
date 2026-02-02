d87186cffecdabd81626a6a51e94924ea90edadb
Alok pradhan <palok2406@gmail.com>
2026-02-02 09:10:35 +0000

    update

--- src/page/Check.jsx
+++ src/page/Check.jsx
@@ -61,34 +61,65 @@ const Check = () => {
     }
 
     return (
-        <div className=' flex flex-col items-center justify-center'>
+        <div className=' flex flex-col items-center justify-center bg-gray-300'>
             <div className='flex justify-center items-center mt-[5vh] w-[100vw] m-4  bg-blue-200 h-[100%] '>
                 <form action="" onSubmit={handleSubmit} className=''>
                     <input type="text"
                         value={(input)}
                         onChange={(e) => setInput(e.target.value)}
-                        className='border-2 border-black p-2 rounded-xl  text-2xl '
+                        className='border-2 border-black mt-5 p-2 rounded-xl  text-2xl '
                     />
-                    <button type='submit' className='mt-5 p-3 rounded-xl md:m-3 bg-blue-400'>
+                    <button type='submit' className='mt-5 p-2 rounded-xl text-2xl md:m-3 bg-blue-400'>
                         Submit
                     </button>
                 </form>
             </div>
-            <div className='flex gap-[10vh] text-3xl mt-4'>
+            <div className='flex gap-[10vh] text-1xl mt-4'>
                 {
                     even ?
                         < div className='row bg-green-400 p-3 rounded-lg'>
-                            <table>
-                                <thead>Even Column</thead>
-
+                            <table className='m-3'>
+                                <thead className='font-bold'>Even Column</thead>
+                                <tr>
+                                    <th className='p-4'>Company</th>
+                                    <th className='p-4'>Contact</th>
+                                    <th className='p-4'>Country</th>
+                                </tr>
+                                <tr>
+                                    <td className='p-4'>Alfreds </td>
+                                    <td className='p-4'>Maria Anders</td>
+                                    <td className='p-4'>Germany</td>
+                                </tr>
+                                <tr>
+                                    <td className='p-4'>Centro </td>
+                                    <td className='p-4'>Francisco Chang</td>
+                                    <td className='p-4'>Mexico</td>
+                                </tr>
                             </table>
                         </div> : ""
                 }
                 {
                     Odd ?
-                        < div className='row bg-blue-400 p-3 rounded-lg'><table>
-                            <thead>Odd Column</thead>
-                        </table></div> : ""
+                        < div className='row bg-blue-400 p-3 rounded-lg'>
+                            <table className='m-3'>
+                                <thead className='font-bold'>Odd Column</thead>
+                                <tr className='p-3 '>
+                                    <th className='p-4'>Company</th>
+                                    <th className='p-4'>Contact</th>
+                                    <th className='p-4'>Country</th>
+                                </tr>
+                                <tr>
+                                    <td className='p-4'>Alfreds </td>
+                                    <td className='p-4'>Maria Anders</td>
+                                    <td className='p-4'>Germany</td>
+                                </tr>
+                                <tr>
+                                    <td className='p-4'>Centro </td>
+                                    <td className='p-4'>Francisco Chang</td>
+                                    <td className='p-4'>Mexico</td>
+                                </tr>
+                            </table>
+                        </div> : ""
 
                 }
                 {
